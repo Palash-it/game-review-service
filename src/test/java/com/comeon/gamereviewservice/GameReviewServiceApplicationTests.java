73a856ce9a2df9172210a3b4b5ce5ec8f2b0f3e7
Palash <palash.debnath@epassi.com>
2024-06-27 23:50:40 +0200

    Initialized spring boot app with a h2 inmem db config

---
+++ src/test/java/com/comeon/gamereviewservice/GameReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeon.gamereviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
